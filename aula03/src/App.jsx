2defbb6b95af801e014a3a4fc952ecf52a6194f0
Jean <jeanlucas2005@hotmail.com>
2024-09-03 19:46:03 -0300

    ok

---
+++ aula03/src/App.jsx
@@ -0,0 +1,13 @@
+import Titulo from './components/Titulo'
+
+function App() {
+  return (
+    <div>
+      {console.log("muit burro bracisco")}
+      <Titulo />
+      <p>minha aplicação</p>
+    </div>
+  )
+}
+
+export default App;
